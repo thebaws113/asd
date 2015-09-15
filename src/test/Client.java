059768c6d399e4f5ad252890d8756adf91684176
thebaws113 <c.marcu21@yahoo.com>
2015-09-15 10:24:25 -0700

    replaced org.json to gson

--- src/test/Client.java
+++ src/test/Client.java
@@ -1,7 +1,5 @@
 package test;
 
- 
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -10,29 +8,35 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
- 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class Client {
+	static String baseUrl = "http://api.openweathermap.org/data/2.5/";
 
 	public static void main(String[] args) throws IOException {
-Client c=new Client();
+		Client c = new Client();
+		String a= "weather?q=London";
+		c.query(a);
+		JsonParser jsonParser = new JsonParser();
+		JsonElement element = jsonParser.parse(c.query(a));
+		System.out.print(element.toString());
 	}
 	
-	Client() throws IOException{
-		 URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=London,uk");
-	      BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-	      BufferedWriter writer = new BufferedWriter(new FileWriter("data.html"));
-	      String line;
-	      while ((line = reader.readLine()) != null) {
-	    	  
-	         System.out.println(line);
-	         writer.write(line);
-	         writer.newLine();
-	      }
-	      reader.close();
-	      writer.close();
-	   	
-	
-
-}
+	public String query (String s) throws IOException {
+		URL url = new URL(baseUrl+s);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+		String str = "" ;
+		String line;
+		while ((line = reader.readLine()) != null) {
+			str = str + line;
+		}
+		
+		reader.close();
+		return str;
+	}
 }
