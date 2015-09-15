bc277c1a4368a0b6d9c39814ca2317c8ef064422
thebaws113 <c.marcu21@yahoo.com>
2015-09-15 14:47:11 -0700

    changes weather

--- src/test/Client.java
+++ src/test/Client.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -12,6 +14,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONStringer;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
@@ -20,23 +24,53 @@ public class Client {
 
 	public static void main(String[] args) throws IOException {
 		Client c = new Client();
-		String a= "weather?q=London";
+		String a = "weather?q=Bucharest";
 		c.query(a);
 		JsonParser jsonParser = new JsonParser();
 		JsonElement element = jsonParser.parse(c.query(a));
-		System.out.print(element.toString());
+
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(element);
+		System.out.println(json);
 	}
-	
-	public String query (String s) throws IOException {
-		URL url = new URL(baseUrl+s);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-		String str = "" ;
-		String line;
-		while ((line = reader.readLine()) != null) {
-			str = str + line;
+
+	public String query(String location) {
+		HttpURLConnection con = null;
+		InputStream is = null;
+
+		try {
+			con = (HttpURLConnection) (new URL(baseUrl + location)).openConnection();
+			con.setRequestMethod("GET");
+			con.setDoInput(true);
+			con.setDoOutput(true);
+			con.connect();
+
+			// Let's read the response
+			StringBuffer buffer = new StringBuffer();
+			is = con.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			String line = null;
+			while ((line = br.readLine()) != null)
+				buffer.append(line + "\r\n");
+
+			is.close();
+			con.disconnect();
+			return buffer.toString();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (Throwable t) {
+			}
+			try {
+				con.disconnect();
+			} catch (Throwable t) {
+			}
 		}
-		
-		reader.close();
-		return str;
+
+		return null;
+
 	}
+
 }
---
+++ src/test/WeatherData.java
@@ -0,0 +1,53 @@
+package test;
+
+public class WeatherData extends abstractWeather {
+
+	
+	WeatherData(){
+		
+	}
+	@Override
+	public float getTemp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getHumidity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getPressure() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getWindSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getWindGust() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getWindDeg() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	 
+
+	@Override
+	public int getPrecipitation() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
